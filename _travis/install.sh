3ecc4741af4c078774a7b354254cda40a24dee46
yiendos <yiendos@sbmatrix.com>
2018-01-04 15:09:05 +0000

    #1: another attempt to get travis to work

--- _travis/install.sh
+++ _travis/install.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-set -e 
+set -e
 
-echo "Testing required dependencies"
-java -version
-google-chrome-stable --version
+##
+# Install the build tools
+##
+
+echo "Install Phing"
+pear channel-discover pear.phing.info
+pear install phing/phing
+phpenv rehash
+
+echo "Set environment variables"
+source $TRAVIS_BUILD_DIR/scripts/travis/env.sh
+
+echo "Configure PHP"
+phpenv config-rm xdebug.ini
+echo 'error_reporting = 22519' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
+echo "Add Pear to include path"
+echo "include_path = \""$(php -r 'echo get_include_path();')":"$(pear config-get php_dir)"\"" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+##
+# Set up Codeception tools
+##
 
 echo "Creating virtual display"
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x8
 sleep 2
 
 echo "Installing Chrome Driver"
-curl -s -L https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip -o /tmp/chromedriver.zip
+curl -s -L https://chromedriver.storage.googleapis.com/2.34/chromedriver_linux64.zip -o /tmp/chromedriver.zip
 unzip /tmp/chromedriver.zip -d /home/travis/
 chmod +x /home/travis/chromedriver
-/home/travis/chromedriver --url-base=/wd/hub &
+/home/travis/chromedriver --disable-impl-side-painting --verbose --log-path=/tmp/chromedriver.log --url-base=/wd/hub &
 
-echo "Setting up the test database"
-mysql -uroot -e "create database IF NOT EXISTS sites_test;"
-mysql -uroot sites_test < tests/_data/dump.sql
 
-echo "Starting MailHog"
-curl -s -L https://github.com/mailhog/MailHog/releases/download/v0.2.0/MailHog_linux_amd64 -o mailhog
-chmod +x mailhog
-./mailhog > /dev/null 2>&1 &
-sleep 1
+echo "Installing the test dependencies"
+mkdir -p $DOCUMENTROOT $PROJECT_DIR
+composer global require --no-interaction codeception/codeception:2.3.4 facebook/webdriver:1.4.1 flow/jsonpath joomlatools/console
+joomla plugin:install joomlatools/console-joomlatools:dev-master
+
+echo "Installing Joomla $JOOMLA"
+joomla site:create --projects-dir=$PROJECT_DIR --release=$JOOMLA --symlink=textman --www=$DOCUMENTROOT --mysql-login="root" test
+
+echo "Starting the PHP webserver"
+cd $DOCUMENTROOT/textman
+php -S localhost:8080 > /dev/null 2>&1 &
+
+cd $TRAVIS_BUILD_DIR
